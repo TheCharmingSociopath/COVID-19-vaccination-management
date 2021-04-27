de5d261c767765e28ceb39ca3e17fca5d592dd09
TheCharmingSociopath <aditya.morolia@research.iiit.ac.in>
2021-04-27 18:38:38 +0530

    started writing huristic based algo for center to states distribution

--- demo/helpers.py
+++ demo/helpers.py
@@ -1,12 +1,57 @@
-def CheckEligibilityHelper(aadhar, name, age):
-    print("age = ",age)
-    if age%2 == 1:
-        return True
-    else:
-        return False
+from models.py import *
+from collections import Counter
 
-def GetCurrentPriority():
+CURRENT_ACTIVE_PRIORITY = 1
+STATES = ['IN-AN', 'IN-AP', 'IN-AR', 'IN-AS', 'IN-BR', 'IN-CH', 'IN-CT', 'IN-DN', 'IN-DL', 'IN-GA', 'IN-GJ', 'IN-HR', 'IN-HP', 'IN-JK', 'IN-JH', 'IN-KA', 'IN-KL', 'IN-LA', 'IN-LD', 'IN-MP', 'IN-MH', 'IN-MN', 'IN-ML', 'IN-MZ', 'IN-NL', 'IN-OR', 'IN-PY', 'IN-PB', 'IN-RJ', 'IN-SK', 'IN-TN', 'IN-TG', 'IN-TR', 'IN-UP', 'IN-UT', 'IN-WB']
+
+def CheckEligibilityHelper(aadhar, district):
+    priority = Population.objects.filter(adhaar=aadhar, deleted=False)
+    if priority <= GetCurrentPriority(district):
+        return True
+    return False
+
+def GetCurrentPriority(district):
+    return CURRENT_ACTIVE_PRIORITY
+
+def UpdatePriority(priority):
+    CURRENT_ACTIVE_PRIORITY = priority
+
+def VaccineAvailabilityInDistrict(district):
+    center_list = [{'id' : center.id, 'address' : center.address} for center in VaccinationCenter.objects.filter(district=district, deleted=False) if center.number_of_vaccines > 0]
+    return center_list
+
+def ReduceVaccineCountAtCenter(center_id):
+    center = VaccinationCenter.objects.get(pk=center_id)
+    center.number_of_vaccines = center.number_of_vaccines - 1
+    state = States.objects.get(pk=center.state)
+    state.number_of_vaccines = state.number_of_vaccines - 1
+    state.number_of_people_vaccinated = state.number_of_people_vaccinated + 1
+    state.save()
+    center.save()
+
+def DistributeCenterToState(number):
+    # weighted ratio of StatewisePopulation, StatewiseVaccinationCenterPopulationRatio, StatewiseInfectionGradient 
     pass
 
-def VaccineAvailabilityInLocation():
+def GetTable():
+    pass
+
+def Initialize():
+    # Update active cases in states database from CSV
+    # Update active cases in districts database from CSV
+    pass
+
+def StatewisePopulation():
+    lst = [person.state.state_code for person in Population.objects.all() if person.priority <= CURRENT_ACTIVE_PRIORITY]
+    return dict(Counter(lst))
+
+def StatewiseVaccinationCenterPopulationRatio(statewise_population):
+    lst = [center.state.state_code for center in VaccinationCenter.objects.all()]
+    vccount = dict(Counter(lst))
+    ret = {}
+    for key in vccount:
+        ret[key] = vccount[key] / statewise_population[key]
+    return ret
+
+def StatewiseInfectionGradient():
     pass
--- demo/models.py
+++ demo/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class States(models.Model):
+    state_codes = (('IN-AN', 'IN-AN'), ('IN-AP', 'IN-AP'), ('IN-AR', 'IN-AR'), ('IN-AS', 'IN-AS'), ('IN-BR', 'IN-BR'), ('IN-CH', 'IN-CH'), ('IN-CT', 'IN-CT'), ('IN-DN', 'IN-DN'), ('IN-DL', 'IN-DL'), ('IN-GA', 'IN-GA'), ('IN-GJ', 'IN-GJ'), ('IN-HR', 'IN-HR'), ('IN-HP', 'IN-HP'), ('IN-JK', 'IN-JK'), ('IN-JH', 'IN-JH'), ('IN-KA', 'IN-KA'), ('IN-KL', 'IN-KL'), ('IN-LA', 'IN-LA'), ('IN-LD', 'IN-LD'), ('IN-MP', 'IN-MP'), ('IN-MH', 'IN-MH'), ('IN-MN', 'IN-MN'), ('IN-ML', 'IN-ML'), ('IN-MZ', 'IN-MZ'), ('IN-NL', 'IN-NL'), ('IN-OR', 'IN-OR'), ('IN-PY', 'IN-PY'), ('IN-PB', 'IN-PB'), ('IN-RJ', 'IN-RJ'), ('IN-SK', 'IN-SK'), ('IN-TN', 'IN-TN'), ('IN-TG', 'IN-TG'), ('IN-TR', 'IN-TR'), ('IN-UP', 'IN-UP'), ('IN-UT', 'IN-UT'), ('IN-WB', 'IN-WB') )
     name = models.CharField(max_length=255)
     number_of_active_cases = models.IntegerField()
+    number_of_vaccines = models.IntegerField()
+    number_of_people_vaccinated = models.IntegerField()
+    state_code = models.CharField(max_length=10, choices=state_codes)
 
 class Districts(models.Model):
     name = models.CharField(max_length=255)
@@ -32,7 +36,7 @@ class VaccinationCenter(models.Model):
     number_of_vaccines = models.IntegerField()
     address = models.CharField(max_length=1023)
 
-class ManufacturingSite(models.Model):
+class CenterVaccinationStore(models.Model):
     district = models.ForeignKey(Districts, on_delete=models.CASCADE)
     state = models.ForeignKey(States, on_delete=models.CASCADE)
     number_of_vaccines = models.IntegerField()
