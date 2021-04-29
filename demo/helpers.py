7efc2b43e3338fc1ae3353c57c1343bbf7719aab
TheCharmingSociopath <aditya.morolia@research.iiit.ac.in>
2021-04-29 13:29:26 +0530

    vaccine distribution huristic

--- demo/helpers.py
+++ demo/helpers.py
@@ -1,8 +1,14 @@
 from models.py import *
 from collections import Counter
+from datetime import timedelta, datetime
 
 CURRENT_ACTIVE_PRIORITY = 1
 STATES = ['IN-AN', 'IN-AP', 'IN-AR', 'IN-AS', 'IN-BR', 'IN-CH', 'IN-CT', 'IN-DN', 'IN-DL', 'IN-GA', 'IN-GJ', 'IN-HR', 'IN-HP', 'IN-JK', 'IN-JH', 'IN-KA', 'IN-KL', 'IN-LA', 'IN-LD', 'IN-MP', 'IN-MH', 'IN-MN', 'IN-ML', 'IN-MZ', 'IN-NL', 'IN-OR', 'IN-PY', 'IN-PB', 'IN-RJ', 'IN-SK', 'IN-TN', 'IN-TG', 'IN-TR', 'IN-UP', 'IN-UT', 'IN-WB']
+LAST_DISPATCH = 0
+PREVIOUS_ACTIVE_CASES = {}
+WEIGHT1 = 0.6 # Ratio of Population
+WEIGHT2 = 0.1 # Ratio of number of vaccination center per unit population
+WEIGHT3 = 0.3 # Gradient of active cases
 
 def CheckEligibilityHelper(aadhar, district):
     priority = Population.objects.filter(adhaar=aadhar, deleted=False)
@@ -17,7 +23,7 @@ def UpdatePriority(priority):
     CURRENT_ACTIVE_PRIORITY = priority
 
 def VaccineAvailabilityInDistrict(district):
-    center_list = [{'id' : center.id, 'address' : center.address} for center in VaccinationCenter.objects.filter(district=district, deleted=False) if center.number_of_vaccines > 0]
+    center_list = {center.id : center.address for center in VaccinationCenter.objects.filter(district=district, deleted=False) if center.number_of_vaccines > 0}
     return center_list
 
 def ReduceVaccineCountAtCenter(center_id):
@@ -31,27 +37,44 @@ def ReduceVaccineCountAtCenter(center_id):
 
 def DistributeCenterToState(number):
     # weighted ratio of StatewisePopulation, StatewiseVaccinationCenterPopulationRatio, StatewiseInfectionGradient 
-    pass
-
-def GetTable():
-    pass
-
-def Initialize():
-    # Update active cases in states database from CSV
-    # Update active cases in districts database from CSV
-    pass
+    statewise_population, r1 = StatewisePopulation()
+    r2 = StatewiseVaccinationCenterPopulationRatio(statewise_population)
+    r3 = StatewiseInfectionGradient()
+    r = { key : WEIGHT1 * r1[key] + WEIGHT2 * r2[key] + WEIGHT3 * r3[key] for key in r1 }
+    print("sum of ratio", sum(list(r.values())))
+    distribution = { key : r[key] * number for key in r }
+    rem = number - sum(list(distribution.values()))
+    print("rem: ", rem)
+    distribution[STATES[-1]] += rem
+    return distribution
 
 def StatewisePopulation():
-    lst = [person.state.state_code for person in Population.objects.all() if person.priority <= CURRENT_ACTIVE_PRIORITY]
-    return dict(Counter(lst))
+    lst = [person.state.state_code for person in Population.objects.all() if person.priority <= CURRENT_ACTIVE_PRIORITY and person.vaccination_status != "vaccinated"]
+    statewise_population = dict(Counter(lst))
+    return statewise_population, Normalise(ret)
 
 def StatewiseVaccinationCenterPopulationRatio(statewise_population):
     lst = [center.state.state_code for center in VaccinationCenter.objects.all()]
     vccount = dict(Counter(lst))
-    ret = {}
-    for key in vccount:
-        ret[key] = vccount[key] / statewise_population[key]
-    return ret
+    ret = { key : vccount[key] / statewise_population[key] for key in vccount}
+    return Normalise(ret)
 
 def StatewiseInfectionGradient():
-    pass
+    number_of_active_cases = {state.state_code : state.number_of_active_cases for state in States.objects.all() }
+
+    PREVIOUS_ACTIVE_CASES = number_of_active_cases
+    if LAST_DISPATCH == 0: # Return the current infection count the first time
+        LAST_DISPATCH = datetime.now()
+        return number_of_active_cases
+    ## Return gradient number of active cases otherwise
+    number_of_days = (datetime.now() - LAST_DISPATCH).days
+    grad = { key : (number_of_active_cases[key] - PREVIOUS_ACTIVE_CASES[key]) / number_of_days for key in STATES }
+    return Normalise(grad)
+
+def Normalise(dick):
+    total = 0
+    for key in dick:
+        total += dick[key]
+    for key in dick:
+        dick[key] /= total
+    return dick
---
+++ initialize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Get the files
+wget "http://api.covid19india.org/csv/latest/cowin_vaccine_data_statewise.csv"
+wget "https://api.covid19india.org/csv/latest/state_wise.csv"
+wget "https://api.covid19india.org/csv/latest/district_wise.csv"
+wget "http://api.covid19india.org/csv/latest/cowin_vaccine_data_districtwise.csv"
+
+## Update States database
+
+## Update Districts database
+
+## Setup dummy VaccinationCenter database
+
+## Setup dummy CenterVaccinationStore database
+
+## Setup dummy Population database
